e84432d6e1a162cf56bde657864f414ce64b7f1f
Ravan G. Huseynov <ravangh@code.local>
2023-05-04 15:35:24 +0400

    2 tasks is done

---
+++ main.js
@@ -0,0 +1,37 @@
+//Task1
+// let numbers = [ -3, 8, 7, 6, 5, -4, 3, 2, 1 ];
+// numbers.sort((a,b) =>a-b)
+// console.log(numbers);
+
+//Task2
+// let arr1=[1, 2, 3];
+// let arr2= [100, 2, 1, 10]
+
+
+
+//Task3
+
+
+
+
+
+
+//Task4
+let library = [
+    { author: 'Bill Gates', title: 'The Road Ahead', libraryID: 1254}, 
+    { author: 'Steve Jobs', title: 'Walter Isaacson', libraryID: 4264}, 
+    { author: 'Suzanne Collins', title: 'Mockingjay: The Final Book of The Hunger Games', libraryID: 3245} 
+    ];
+function myFunc(a,b){
+    if(a.title<b.title){
+        return -1;
+    }
+    if(a.title>b.title){
+        return 1;
+    }
+}
+   
+ 
+
+
+
