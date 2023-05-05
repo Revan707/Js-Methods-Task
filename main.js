a1457f48139ceeacaa0106dfab8cd89b7cbc33de
Revan707 <ravangh@code.edu.az>
2023-05-05 10:41:57 +0400

    Js methods task is done

--- main.js
+++ main.js
@@ -6,30 +6,56 @@
 //Task2
 // let arr1=[1, 2, 3];
 // let arr2= [100, 2, 1, 10]
-
+// let union = [...new Set([...arr1,...arr2])];
+// let res=union.sort((a,b)=>a-b);
+// console.log(union);
 
 
 //Task3
-
-
-
+// let arr=[NaN, 0, 15, false, -22, '',undefined, 47, null]
+// let res=arr.filter(myFunc);
+// function myFunc(value){
+//     return !isNaN(value)&value!=0&value!=Boolean&value!=undefined&value!=null ;
+// }
+// console.log(res);
 
 
 
 //Task4
-let library = [
-    { author: 'Bill Gates', title: 'The Road Ahead', libraryID: 1254}, 
-    { author: 'Steve Jobs', title: 'Walter Isaacson', libraryID: 4264}, 
-    { author: 'Suzanne Collins', title: 'Mockingjay: The Final Book of The Hunger Games', libraryID: 3245} 
-    ];
-function myFunc(a,b){
-    if(a.title<b.title){
-        return -1;
-    }
-    if(a.title>b.title){
-        return 1;
-    }
-}
+// let library = [
+//     { author: 'Bill Gates', title: 'The Road Ahead', libraryID: 1254}, 
+//     { author: 'Steve Jobs', title: 'Walter Isaacson', libraryID: 4264}, 
+//     { author: 'Suzanne Collins', title: 'Mockingjay: The Final Book of The Hunger Games', libraryID: 3245} 
+//     ];
+// function myFunc(a, b) {
+//     if (a.title < b.title) return -1;
+//     if (a.title > b.title) return 1;
+// }
+// console.log(library.sort(myFunc));
+
+
+
+// //Task5
+// let arr=[2,4,9,16,32,];
+// function myFunc() {
+//     for (let i = 0; i < arr.length-1; i++) {
+//         if(arr[i+1]%arr[i]!=0)return false;
+//     }
+//     return true
+    
+// }
+// console.log(myFunc());
+
+
+
+
+
+
+
+
+
+
+
    
  
 
